74414ee118f5eb6db15290a629f4181c4dd5a065
Manik Surtani <manik@surtani.org>
2016-02-09 15:21:03 -0800

    Get protoc

--- bin/compile_protos.sh
+++ bin/compile_protos.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-QS_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
-
-protoc --go_out=plugins=grpc:. $QS_HOME/protos/*.proto --proto_path $QS_HOME
+protoc --go_out=plugins=grpc:. ./protos/*.proto --proto_path ./
