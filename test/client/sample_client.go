9139e8e7288d2122f5eeb7ea36e661871af0db79
Manik Surtani <manik@surtani.org>
2016-03-06 16:25:28 -0800

    move client

---
+++ test/client/sample_client.go
@@ -0,0 +1,39 @@
+// Licensed under the Apache License, Version 2.0
+// Details: https://raw.githubusercontent.com/maniksurtani/quotaservice/master/LICENSE
+
+package main
+
+import (
+	"fmt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/grpclog"
+	"golang.org/x/net/context"
+	pb "github.com/maniksurtani/quotaservice/protos"
+	"github.com/golang/protobuf/proto"
+)
+
+func main() {
+	fmt.Println("Starting example client.")
+	serverAddr := "127.0.0.1:10990"
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithInsecure())
+	conn, err := grpc.Dial(serverAddr, opts...)
+	if err != nil {
+		grpclog.Fatalf("fail to dial: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewQuotaServiceClient(conn)
+
+	req := &pb.AllowRequest{
+		Namespace: proto.String("test.namespace"),
+		Name: proto.String("abc"),
+		NumTokensRequested: proto.Int64(1)}
+	rsp, err := client.Allow(context.TODO(), req)
+	if err != nil {
+		fmt.Printf("Caught error %v", err)
+	} else {
+		fmt.Printf("Got response %v", rsp)
+	}
+}
+
