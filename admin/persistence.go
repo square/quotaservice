40f5a7334858e447cb9dbe456fa19012b27fa411
Manik Surtani <manik@surtani.org>
2016-03-14 22:11:43 -0700

    Updated docs

--- admin/persistence.go
+++ admin/persistence.go
@@ -11,18 +11,26 @@ import (
 	pb "github.com/maniksurtani/quotaservice/protos/config"
 )
 
-// ConfigPersister blah blah
+// ConfigPersister is an interface that persists configs and notifies a channel of changes.
 type ConfigPersister interface {
+	// PersistAndNotify persists the configuration passed in, returning any errors encountered.
 	PersistAndNotify(c *pb.ServiceConfig) error
+	// ConfigChangedWatcher returns a channel that is notified whenever configuration changes are
+	// detected. Changes are coalesced so that a single notification may be emitted for multiple
+	// changes.
 	ConfigChangedWatcher() chan struct{}
+	// ReadPersistedConfig reads configuration previously persisted, returning the configuration 
+    // read and any errors encountered.
 	ReadPersistedConfig() (*pb.ServiceConfig, error)
 }
 
+// DiskConfigPersister is a ConfigPersister that saves configs to the local filesystem.
 type DiskConfigPersister struct {
 	location string
 	watcher  chan struct{}
 }
 
+// NewDiskConfigPersister creates a new DiskConfigPersister
 func NewDiskConfigPersister(location string) (ConfigPersister, error) {
 	_, e := os.Stat(location)
 
@@ -32,13 +40,11 @@ func NewDiskConfigPersister(location string) (ConfigPersister, error) {
 		return nil, e
 	}
 
-	// TODO(manik) test that the location is writeable.
-
 	return &DiskConfigPersister{location, make(chan struct{}, 1)}, nil
 }
 
+// PersistAndNotify persists the configuration passed in, returning any errors encountered.
 func (d *DiskConfigPersister) PersistAndNotify(c *pb.ServiceConfig) error {
-	// Write to disk
 	f, e := os.OpenFile(d.location, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, os.ModePerm)
 	if e != nil {
 		return e
@@ -66,6 +72,8 @@ func (d *DiskConfigPersister) PersistAndNotify(c *pb.ServiceConfig) error {
 	return nil
 }
 
+// ReadPersistedConfig reads configuration previously persisted, returning the configuration read and 
+// any errors encountered.
 func (d *DiskConfigPersister) ReadPersistedConfig() (*pb.ServiceConfig, error) {
 	bytes, e := ioutil.ReadFile(d.location)
 	if e != nil {
@@ -81,6 +89,9 @@ func (d *DiskConfigPersister) ReadPersistedConfig() (*pb.ServiceConfig, error) {
 	return c, nil
 }
 
+// ConfigChangedWatcher returns a channel that is notified whenever configuration changes are
+// detected. Changes are coalesced so that a single notification may be emitted for multiple
+// changes.
 func (d *DiskConfigPersister) ConfigChangedWatcher() chan struct{} {
 	return d.watcher
 }
