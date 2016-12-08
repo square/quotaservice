2a54d8120794e188c9c5e8e5031ec60a82cfcc44
Steven Davidovitz <steved@users.noreply.github.com>
2016-12-08 10:11:38 -0800

    make sure the current version is always incremented (#37)
    Otherwise it can be overwritten when hitting POST /api/namespace

--- server_test.go
+++ server_test.go
@@ -5,6 +5,7 @@ package quotaservice
 
 import (
 	"testing"
+	"time"
 
 	"github.com/maniksurtani/quotaservice/config"
 	"github.com/maniksurtani/quotaservice/test/helpers"
@@ -20,4 +21,46 @@ func TestValidServer(t *testing.T) {
 	s := New(&MockBucketFactory{}, &config.MemoryConfigPersister{}, &MockEndpoint{})
 	s.Start()
 	defer s.Stop()
+}
+
+func TestUpdateConfig(t *testing.T) {
+	s := New(&MockBucketFactory{}, config.NewMemoryConfigPersister(), &MockEndpoint{}).(*server)
+
+	originalConfig := config.NewDefaultServiceConfig()
+	originalConfig.Version = 2
+	originalConfig.Date = time.Now().Unix() - 10
+	s.createBucketContainer(originalConfig)
+
+	s.Start()
+	defer s.Stop()
+
+	newConfig := config.NewDefaultServiceConfig()
+
+	if err := s.UpdateConfig(newConfig, "test"); err != nil {
+		t.Fatal("Error when updating config", err)
+	}
+
+	start := time.Now()
+
+	for s.Configs() == originalConfig {
+		if time.Since(start) > time.Second {
+			t.Fatal("Timeout waiting for config to change!")
+		}
+
+		time.Sleep(time.Millisecond * 5)
+	}
+
+	cfg := s.Configs()
+
+	if cfg.User != "test" {
+		t.Errorf("User %+v does not match passed in user \"test\"", s.cfgs.User)
+	}
+
+	if cfg.Version != 3 {
+		t.Errorf("Version %+v does not match current version: 3", s.cfgs.Version)
+	}
+
+	if cfg.Date <= originalConfig.Date {
+		t.Errorf("Date %+v was not updated from %+v", s.cfgs.Date, originalConfig.Date)
+	}
 }
