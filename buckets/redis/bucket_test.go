a0e89154833bfb7240533e877ffa761d25db6579
Manik Surtani <manik@surtani.org>
2017-05-18 12:15:45 -0700

    Reduce memory footprint for dynamic buckets by sharing common values across all dynamic buckets created from the same template (#66)

--- buckets/redis/bucket_test.go
+++ buckets/redis/bucket_test.go
@@ -9,14 +9,21 @@ import (
 
 	"gopkg.in/redis.v5"
 
-	"github.com/maniksurtani/quotaservice"
+	"fmt"
+
 	"github.com/maniksurtani/quotaservice/buckets"
 	"github.com/maniksurtani/quotaservice/config"
+	"github.com/maniksurtani/quotaservice/protos/config"
 )
 
-var cfg = config.NewDefaultServiceConfig()
-var factory quotaservice.BucketFactory
-var bucket *redisBucket
+const (
+	dynMaxDebtMillis = 777
+)
+
+var cfg *quotaservice_configs.ServiceConfig
+var factory *bucketFactory
+var bucket *staticBucket
+var dynMaxDebtNanos = fmt.Sprintf("%v", dynMaxDebtMillis*1000000)
 
 func TestMain(m *testing.M) {
 	setUp()
@@ -25,9 +32,17 @@ func TestMain(m *testing.M) {
 }
 
 func setUp() {
-	factory = NewBucketFactory(&redis.Options{Addr: "localhost:6379"}, 2)
+	dynNs := config.NewDefaultNamespaceConfig("dynNs")
+	dynTpl := config.NewDefaultBucketConfig(config.DynamicBucketTemplateName)
+	dynTpl.MaxDebtMillis = dynMaxDebtMillis
+	config.SetDynamicBucketTemplate(dynNs, dynTpl)
+
+	cfg = config.NewDefaultServiceConfig()
+	config.AddNamespace(cfg, dynNs)
+
+	factory = NewBucketFactory(&redis.Options{Addr: "localhost:6379"}, 2).(*bucketFactory)
 	factory.Init(cfg)
-	bucket = factory.NewBucket("redis", "redis", config.NewDefaultBucketConfig(""), false).(*redisBucket)
+	bucket = factory.NewBucket("redis", "redis", config.NewDefaultBucketConfig(""), false).(*staticBucket)
 }
 
 func TestScriptLoaded(t *testing.T) {
@@ -67,4 +82,63 @@ func TestTokenAcquisition(t *testing.T) {
 
 func TestGC(t *testing.T) {
 	buckets.TestGC(t, factory, "redis")
+}
+
+func TestRefCountsForDynamic(t *testing.T) {
+	assertNoSharedAttribs(t)
+
+	// Create a dynamic bucket - the cast will ensure it is the right type
+	b1 := factory.NewBucket("dynNs", "b1", cfg.Namespaces["dynNs"].DynamicBucketTemplate, true).(*dynamicBucket)
+	assertRefCounts("dynNs", 1, t)
+	if b1.maxDebtNanos != dynMaxDebtNanos {
+		t.Fatalf("Expected maxDebtNanos on dynamic bucket to be %v but was %v", dynMaxDebtNanos, b1.maxDebtNanos)
+	}
+
+	b2 := factory.NewBucket("dynNs", "b2", cfg.Namespaces["dynNs"].DynamicBucketTemplate, true).(*dynamicBucket)
+	assertRefCounts("dynNs", 2, t)
+
+	// Check that b1 and b2 point to the same shared attributes
+	if b1.abstractBucket.configAttributes != b2.abstractBucket.configAttributes {
+		t.Fatalf("b1 and b2 point to different configAttributes. b1 points to %p and b2 points to %p", b1.abstractBucket.configAttributes, b2.abstractBucket.configAttributes)
+	}
+
+	b2.Destroy()
+	assertRefCounts("dynNs", 1, t)
+
+	b3 := factory.NewBucket("dynNs", "b3", cfg.Namespaces["dynNs"].DynamicBucketTemplate, true).(*dynamicBucket)
+	// Check that b1 and b3 point to the same shared attributes
+	if b1.abstractBucket.configAttributes != b3.abstractBucket.configAttributes {
+		t.Fatalf("b1 and b3 point to different configAttributes. b1 points to %p and b3 points to %p", b1.abstractBucket.configAttributes, b3.abstractBucket.configAttributes)
+	}
+
+	assertRefCounts("dynNs", 2, t)
+
+	b1.Destroy()
+	b3.Destroy()
+
+	assertNoSharedAttribs(t)
+}
+
+func assertNoSharedAttribs(t *testing.T) {
+	if len(factory.sharedAttributes) != 0 {
+		t.Fatalf("Expected no shared attributes. Found %+v and refcounts %+v", factory.sharedAttributes, factory.refcounts)
+	}
+
+	if len(factory.refcounts) != 0 {
+		t.Fatalf("Expected no references to shared attributes. Found %+v", factory.refcounts)
+	}
+}
+
+func assertRefCounts(ns string, expected int, t *testing.T) {
+	if _, exists := factory.sharedAttributes[ns]; !exists {
+		t.Fatalf("Expected shared attributes for namespace %v but found %+v", ns, factory.sharedAttributes)
+	}
+
+	if counts, exists := factory.refcounts[ns]; !exists {
+		t.Fatalf("Expected ref counts for namespace %v but found %+v", ns, factory.refcounts)
+	} else {
+		if counts != expected {
+			t.Fatalf("Expected ref counts for namespace %v to be %v but found %v", ns, expected, counts)
+		}
+	}
 }
