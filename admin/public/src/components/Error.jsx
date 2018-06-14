6ea72b333b673436210376674ff0fe0d2b1bb951
Alex Gorbatchev <alex.gorbatchev@gmail.com>
2018-06-14 15:56:21 -0700

    Updates major dependencies except Webpack.

--- admin/public/src/components/Error.jsx
+++ admin/public/src/components/Error.jsx
@@ -1,20 +1,19 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { Component } from 'react';
-import { ApiError, InternalError, RequestError } from 'redux-api-middleware';
 
 export default class Error extends Component {
   renderError(error) {
-    switch (error.constructor) {
-      case RequestError:
+    switch (error.name) {
+      case 'RequestError':
         return `A network error occurred: "${error.message}"`
-      case ApiError:
+      case 'ApiError':
         if (error.response) {
           return error.response.description
         } else {
           return `An error occurred: "${error.statusText}"`
         }
-      case InternalError:
+      case 'InternalError':
       default:
         return 'An unknown error occurred. Please contact your friendly QuotaService owners for help.'
     }
