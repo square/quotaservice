78aff478ce375f16025c49a0627b6b99848e44e4
x48zhu <zhuxi28@hotmail.com>
2024-06-17 11:50:22 -0700

    Print error to browser console

--- admin/public/src/components/Error.jsx
+++ admin/public/src/components/Error.jsx
@@ -6,16 +6,16 @@ export default class Error extends Component {
   renderError(error) {
     switch (error.name) {
       case 'RequestError':
-        return `A network error occurred: "${error.message}"`
+        return `A network error occurred: "${error.message}"`;
       case 'ApiError':
         if (error.response) {
-          return error.response.description
-        } else {
-          return `An error occurred: "${error.statusText}"`
+          return error.response.description;
         }
+        return `An error occurred: "${error.statusText}"`;
       case 'InternalError':
       default:
-        return 'An unknown error occurred. Please contact your friendly QuotaService owners for help.'
+        console.log(error.message);
+        return 'An unknown error occurred. Please contact your friendly QuotaService owners for help.';
     }
   }
 
